4e9229dfccf3e4688dfb608c5ff47278ea27ffd2
2513635177@qq.com <fjgJade>
2023-08-18 19:36:05 +0800

    fib

--- binarySearch.py
+++ binarySearch.py
@@ -75,40 +75,37 @@
 #     keys = 0
 #     print(binary_search(array,keys))
 
-#
-# def binary_search(num, n):
-#     left = 0
-#     right = len(num) - 1
-#
-#     while left <= right:
-#         mid = (left + right) // 2
-#
-#         if num[mid] == n:
-#             return mid
-#
-#         if num[left] <= num[mid]:  # 判断左一半是不是升序的
-#
-#             if num[left] <= n < num[mid]:  # 如果是升序的话 判断目标是否在 left mid 里面
-#                 right = mid - 1  # 在里面的话更新 right
-#             else:  # 虽然是升序 但目标不在这里面
-#                 left = mid + 1  # 更新 left 直接舍去这部分
-#
-#         else:  # 右一半是升序的
-#
-#             if num[mid] < n <= num[right]:  # 判断目标是否在 mid right 里面
-#                 left = mid + 1  # 在里面更新 left
-#             else:  # 目标不在这里面 更新 right
-#                 right = mid - 1
-#
-#     return -1
-#
-#
-# if __name__=='__main__':
-#     array = [-1,0,3,5,9,12]
-#     keys = 9
-#     print(binary_search(array,keys))
+
+def binary_search(num, n):
+    left = 0
+    right = len(num) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if num[mid] == n:
+            return mid
+
+        if num[left] <= num[mid]:  # 判断左一半是不是升序的
+
+            if num[left] <= n < num[mid]:  # 如果是升序的话 判断目标是否在 left mid 里面
+                right = mid - 1  # 在里面的话更新 right
+            else:  # 虽然是升序 但目标不在这里面
+                left = mid + 1  # 更新 left 直接舍去这部分
+
+        else:  # 右一半是升序的
+
+            if num[mid] < n <= num[right]:  # 判断目标是否在 mid right 里面
+                left = mid + 1  # 在里面更新 left
+            else:  # 目标不在这里面 更新 right
+                right = mid - 1
+
+    return -1
 
 
+if __name__=='__main__':
+    array = [-1,0,3,5,9,12]
+    keys = 9
+    print(binary_search(array,keys))
 
-students = [['小明','小红','小刚','小美'],['小强','小兰','小伟','小芳']]
-print(students[1][3])
+
