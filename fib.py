8ae3c496035505715d1eb17f43e11beccd1eb134
2513635177@qq.com <fjgJade>
2023-08-19 10:55:44 +0800

    'fib'

--- fib.py
+++ fib.py
@@ -1,10 +1,24 @@
-def fib(n):
+# def fib(n):
+#     a,b = 0,1
+#     while a < n:
+#         print(a,end=' ')
+#         a,b  = b, a+b
+#     print()
+#
+# fib(10)
+
+
+def fib2(n):
+    """return a list containing the Fibonacci series up to n."""
+    result = []
     a,b = 0,1
     while a < n:
-        print(a,end=' ')
-        a,b  = b, a+b
-    print()
+        result.append(a)
+        a,b = b,a+b
+    return result
+
+print(fib2(100))
 
 
 
-fib(10)
+
