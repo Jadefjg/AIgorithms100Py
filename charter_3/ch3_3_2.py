cf23dbce5a3e2ebe31214492651985654d7dcb2d
2513635177@qq.com <fjgJade>
2023-10-18 01:42:22 +0800

    '优化ch3_3_2'

--- charter_3/ch3_3_2.py
+++ charter_3/ch3_3_2.py
@@ -19,32 +19,33 @@
 """
 
 
-def shuixianhua(x,y):
-    for n in range(x,y):
-        hun = n // 100  # 百位
-        ten = (n - hun * 100) // 10   # 十位
-        ind = n % 10    # 个位
-        m = hun*hun*hun + ten*ten*ten* + ind*ind*ind  # 求和
-        if n == m:
-            print("%d \t" %n , end=" ")
-        return n
+# def shuixianhua(x,y):
+#     for n in range(x,y):
+#         hun = n // 100  # 百位
+#         ten = (n - hun * 100) // 10   # 十位
+#         ind = n % 10    # 个位
+#         m = hun*hun*hun + ten*ten*ten* + ind*ind*ind  # 求和
+#         if n == m:
+#             print("%d \t" %n , end=" ")
+#         return n
+#
+#
+# if __name__ == '__main__':
+#     result = shuixianhua(100, 1000)
+#     print(result)
 
 
 if __name__ == '__main__':
-    result = shuixianhua(100, 1000)
-    print(result)
-
-
-# if __name__ == '__main__':
-#     print("result is: ")
-#     # 整数的取值范围
-#     for n in range(100, 1000):
-#         hun = n // 100 # 百位
-#         ten = (n - hun * 100) // 10 # 十位
-#         ind = n % 10 # 个位
-#         m = hun*hun*hun + ten*ten*ten + ind*ind*ind # 求和
-#         if n == m: # 各位上的立方和是否与原数n相等
-#             print("%d \t" %n, end=" ")
+    print("result is: ")
+    # 整数的取值范围
+    for n in range(100, 1000):
+        hun = n // 100 # 百位
+        ten = (n - hun * 100) // 10 # 十位
+        ind = n % 10 # 个位
+        # m = hun*hun*hun + ten*ten*ten + ind*ind*ind # 求和
+        m = hun**3 + ten**3 + ind**3
+        if n == m: # 各位上的立方和是否与原数n相等
+            print("%d \t" %n, end=" ")
 
 
 
